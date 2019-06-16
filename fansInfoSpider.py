83254b519662600018961853a3e45ddf74973cd9
Billdex <591404144@qq.com>
2019-06-16 23:54:27 +0800

    将请求html和获取用户信息这类常用函数放到spiderUtils.py以方便复用

--- fansInfoSpider.py
+++ fansInfoSpider.py
@@ -3,37 +3,7 @@ import json
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
-# 请求文本
-def getHtmlText(url, code='UTF-8'):
-    trytime = 5
-    while trytime > 0:
-        try:
-            header = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6726.400 QQBrowser/10.2.2265.400',
-            }
-            r = requests.get(url, headers=header, timeout=5)
-            r.raise_for_status()
-            r.encoding = code
-            return r.text
-        except:
-            print("get获取失败,重连中")
-            trytime -= 1
-
-def getUserInfo(uid):
-    url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value={}'.format(uid)
-    Infomation = json.loads(getHtmlText(url))
-    UserInfo = {}
-    UserInfo['id'] = Infomation['data']['userInfo']['id']
-    UserInfo['name'] = Infomation['data']['userInfo']['screen_name']
-    UserInfo['gender'] = '女' if Infomation['data']['userInfo']['gender'] == 'f' else '男'
-    UserInfo['statuses_count'] = Infomation['data']['userInfo']['statuses_count']
-    UserInfo['desc'] = Infomation['data']['userInfo']['description']
-    UserInfo['fans_count'] = Infomation['data']['userInfo']['followers_count']
-    UserInfo['follow_count'] = Infomation['data']['userInfo']['follow_count']
-    # with open('./UserInfo.json', 'w', encoding='utf-8') as f:
-    #     f.write(json.dumps(UserInfo, ensure_ascii=False))
-    return UserInfo
+from spiderUtils import *
 
 def getFansList(uid, num):
     page = 1
--- interestListSpider.py
+++ interestListSpider.py
@@ -4,36 +4,7 @@ import json
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
-# 请求文本
-def getHtmlText(url, code='UTF-8'):
-    trytime = 5
-    while trytime > 0:
-        try:
-            header = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6726.400 QQBrowser/10.2.2265.400',
-            }
-            r = requests.get(url, headers=header, timeout=5)
-            r.raise_for_status()
-            r.encoding = code
-            return r.text
-        except:
-            print("get获取失败,重连中")
-            trytime -= 1
-
-
-# 获取用户信息
-def getUserInfo(uid):
-    url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value={}'.format(uid)
-    Infomation = json.loads(getHtmlText(url))
-    UserInfo = {}
-    UserInfo['id'] = Infomation['data']['userInfo']['id']
-    UserInfo['name'] = Infomation['data']['userInfo']['screen_name']
-    UserInfo['gender'] = '女' if Infomation['data']['userInfo']['gender'] == 'f' else '男'
-    UserInfo['desc'] = Infomation['data']['userInfo']['description']
-    # with open('./UserInfo.json', 'w', encoding='utf-8') as f:
-    #     f.write(json.dumps(UserInfo, ensure_ascii=False))
-    return UserInfo
+from spiderUtils import *
 
 
 # 获取用户关注列表
